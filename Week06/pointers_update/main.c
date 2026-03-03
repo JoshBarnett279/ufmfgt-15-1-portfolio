7cb80eff0535e27aa973191458c410845015f7de
jw2-barnett <joshua.barnett279@gmail.com>
2026-03-03 19:05:50 +0000

    Week 6: Demonstrated stack memory reuse with local variables

--- Week06/pointers_update/main.c
+++ Week06/pointers_update/main.c
@@ -5,23 +5,22 @@
 void foo1(int xval){
     int x;
     x = xval;
-/* print the address of x here – “The address of x is ..” */
+    printf("The address of x is %p\n\n", (void*)&x);
 }
 void foo2(int yval){
     int y;
     y = yval;
-/* print the address of y here – “The address of y is..” */
+    printf("The address of y is %p\n\n", (void*)&y);
 }
-/*
-void foo3(void){
-int z;
-foo1(z);
+void foo3(int zval){
+    int z;
+    z = zval;
+    printf("The address of z is %p\n\n", (void*)&z);
 }
-*/
 int main(void){
     foo1(7);
     foo2(11);
-/* foo3(); */
+    foo3(16);
     system("PAUSE");
     return 0;
 }
